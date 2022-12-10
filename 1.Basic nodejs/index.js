3e69e71877e922528322f14e03eed7d1af060123
ASHUTOSH KUMAR CHOUDHARY <choudharykumarashutosh1209@gmail.com>
2022-12-10 15:02:19 +0530

    basic of node js added

---
+++ 1.Basic nodejs/index.js
@@ -0,0 +1,21 @@
+//node is js runtime environment
+//run file in node js->(node file_name)->node index.js
+//server side environment
+console.log("Hi Js");
+
+//while running js in script(cmd line)
+//->we get undefined because the statement are not returning anything.
+
+var a = 10;
+var b = 20;
+var c = 30;
+console.warn(a * b * c);
+
+//The node:console module provides a simple debugging console 
+//that is similar to the JavaScript console mechanism provided by web browsers.
+
+//3 types of console statements
+//console.log
+//console.warn
+//console.error
+
